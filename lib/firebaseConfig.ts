37f08e2b9e86d3c659dab5678a26d19c688a8fbb
Moin <154509081+moin04-newbie@users.noreply.github.com>
2025-07-27 22:25:51 +0530

    Update firebaseConfig.ts

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: "",
-  authDomain: "scrapconnect-7877a.firebaseapp.com",
-  projectId: "scrapconnect-7877a",
-  storageBucket: "scrapconnect-7877a.firebasestorage.app",
-  messagingSenderId: "498542674850",
-  appId: "1:498542674850:web:cc5024c22e15a0771ee8fb",
-  measurementId: "G-ELB3PJGH0F"
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
